494b50eed1cf58cc66535e212971d4ac42eb0a51
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-02-26 08:54:20 +0100

    Update package.py

--- var/spack/repos/builtin/packages/kokkos-fft/package.py
+++ var/spack/repos/builtin/packages/kokkos-fft/package.py
@@ -50,7 +50,7 @@ class KokkosFft(CMakePackage):
         args = [
             self.define("KokkosFFT_ENABLE_INTERNAL_KOKKOS", False),
             self.define_from_variant("KokkosFFT_ENABLE_HOST_AND_DEVICE", "host"),
-            self.define("KokkosFFT_ENABLE_TESTS", "unit_test"),
+            self.define_from_variant("KokkosFFT_ENABLE_TESTS", "unit_test"),
             self.define("KokkosFFT_ENABLE_DOCS", False),
             self.define("KokkosFFT_ENABLE_BENCHMARK", False),
             self.define("KokkosFFT_ENABLE_EXAMPLES", False),
